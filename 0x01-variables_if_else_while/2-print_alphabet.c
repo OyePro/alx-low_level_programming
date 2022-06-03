eb4ff6b313f24b2d0356875e6c496904f38d464e
Olalekan Okewale <olalekanokewale2020@gmail.com>
2022-06-03 21:39:58 +0000

    Lower case alphabet

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+char lcase;
+char nl;
+nl = 012;
+for (lcase = 'a' ; lcase <= 'z' ; lcase++)
+{
+putchar(lcase);
+}
+putchar(nl);
+return (0);
+}
