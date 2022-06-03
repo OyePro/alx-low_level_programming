6fc990435f49e227e899475c79fb9983f01f58f8
Olalekan Okewale <olalekanokewale2020@gmail.com>
2022-06-03 23:22:04 +0000

    Numberz in decimal

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+int Dec;
+for (Dec = 48 ; Dec <= 57 ; Dec++)
+{
+putchar(Dec);
+}
+putchar('\n');
+return (0);
+}
