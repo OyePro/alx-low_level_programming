40a58719130adfeb5583ef7728d56e34d87e4211
Olalekan Okewale <olalekanokewale2020@gmail.com>
2022-06-07 21:17:19 +0000

    _putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,20 +1,28 @@
-#include <stdio.h>
-#include <time.h>
+#include "main.h"
 /**
- * main - Entry point
+ * main - Entry point into print _putchar
  *
  * Return: Always 0 (success)
  */
 int main(void)
 {
-putchar(95);
-putchar(112);
-putchar(117);
-putchar(116);
-putchar(99);
-putchar(104);
-putchar(97);
-putchar(114);
-putchar(10);
+char _, p, u, t, c, h, a, r;
+_ = 95;
+p = 112;
+u = 117;
+t = 116;
+c = 99;
+h = 104;
+a = 97;
+r = 114;
+_putchar(_);
+_putchar(p);
+_putchar(u);
+_putchar(t);
+_putchar(c);
+_putchar(h);
+_putchar(a);
+_putchar(r);
+_putchar('\n');
 return (0);
 }
---
+++ 0x02-functions_nested_loops/main.h
@@ -0,0 +1,27 @@
+#ifndef MAIN_H
+#define MAIN_H
+/**
+ * _putchar - writes the character c to the stdout
+ * @c: character to print
+ *
+ * Return: 1 (success)
+ * On error, return -1
+ */
+int _putchar(char);
+/**
+ * _putchar - print alphabet in lower case
+ *
+ * Return: void
+ */
+void print_alphabet(void);
+void print_alphabet_x10(void);
+int _islower(int c);
+int _isalpha(int c);
+int print_sign(int n);
+int _abs(int);
+int print_last_digit(int);
+void jack_bauer(void);
+void times_table(void);
+int add(int, int);
+void print_to_98(int n);
+#endif
