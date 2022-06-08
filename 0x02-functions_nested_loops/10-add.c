8c717ee98e4384eb717202890786a522c84b7b08
Olalekan Okewale <olalekanokewale2020@gmail.com>
2022-06-08 02:55:40 +0000

    adding two numbers

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * add - function for adding two sum
+ * @c: the integer
+ * @d: the second integer
+ *
+ * Return: sum
+ */
+int add(int c, int d)
+{
+int sum;
+sum = c + d;
+return (sum);
+}
