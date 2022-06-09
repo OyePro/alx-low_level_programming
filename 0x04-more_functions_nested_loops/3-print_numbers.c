b317180c68d8468c1cd3e43e2d96843385152dca
Olalekan Okewale <olalekanokewale2020@gmail.com>
2022-06-09 12:40:54 +0000

    numbers

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * print_numbers - printing number from 0 - 9
+ *
+ *
+ * Return: void
+ */
+void print_numbers(void)
+{
+int n;
+n = 48;
+while (n < 58)
+{
+_putchar(n);
+n++;
+}
+_putchar('\n');
+}
