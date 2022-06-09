f582de39a589521822f4713df9a54a9db1ec8ddf
Olalekan Okewale <olalekanokewale2020@gmail.com>
2022-06-09 12:26:39 +0000

    Multiplication

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * mul - functions to determine the multiplication of a and b
+ * @a: the first integer
+ * @b: the second integer
+ *
+ * Return: sum
+ */
+int mul(int a, int b)
+{
+int mul;
+mul = (a * b);
+return (mul);
+}
