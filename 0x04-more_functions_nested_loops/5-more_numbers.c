5216c6c26b61dc0df1b6828e49dc322b37f9831a
Olalekan Okewale <olalekanokewale2020@gmail.com>
2022-06-09 15:43:37 +0000

    more numbers

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * more_numbers - printing numbers above 9
+ *
+ * Return: void
+ */
+void more_numbers(void)
+{
+int n, m;
+for (n = 0 ; n <= 9 ; n++)
+{
+for (m = 0 ; m <= 14 ; m++)
+{
+if (m >= 10)
+{
+_putchar((m / 10) + 48);
+_putchar((m % 10) + 48);
+}
+}
+_putchar(10);
+}
+}
