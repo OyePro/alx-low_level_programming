500c7b18608fea02b53ea314c467fb88fb4e66cf
Olalekan Okewale <olalekanokewale2020@gmail.com>
2022-06-09 11:40:01 +0000

    Uppercase

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * _isupper - functions to check for upper case in character c
+ * @c: character that is checked
+ *
+ * Return: 0
+ */
+int _isupper(int c)
+{
+if (c == 65 && c <= 90)
+{
+return (1);
+}
+else
+{
+return (0);
+}
+_putchar(10);
+}
---
+++ 0x04-more_functions_nested_loops/main.h
@@ -0,0 +1,21 @@
+#ifndef MAIN_H
+#define MAIN_H
+/**
+ * _putchar - writes the character c to the stdout
+ * @c: character to print
+ *
+ * Return: 1 (success)
+ * On error, return -1
+ */
+int _putchar(char);
+int _isupper(int c);
+int _isdigit(int c);
+int mul(int a, int b);
+void print_numbers(void);
+void print_most_numbers(void);
+void more_numbers(void);
+void print_line(int n);
+void print_diagonal(int n);
+void print_square(int size);
+void print_triangle(int size);
+#endif
