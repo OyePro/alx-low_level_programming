37a1a683a7ddb8c5ba0f6a68146177f92118e0fb
Olalekan Okewale <olalekanokewale2020@gmail.com>
2022-06-09 11:50:46 +0000

    digit

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * _isdigit - function to check if c is a digit
+ * @c: integer to check
+ *
+ * Return: 0
+ */
+int _isdigit(int c)
+{
+if (c == 48 && c <= 57)
+{
+return (1);
+}
+else
+{
+return (0);
+}
+_putchar(10);
+}
