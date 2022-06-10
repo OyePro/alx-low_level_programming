d3c4ae05b43c32a1397a040d5b9de7973f0225de
Olalekan Okewale <olalekanokewale2020@gmail.com>
2022-06-10 00:47:32 +0000

    more numbers

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -9,13 +9,11 @@ void more_numbers(void)
 int n, m;
 for (n = 0 ; n <= 9 ; n++)
 {
-for (m = 0 ; m <= 14 ; m++)
+for (m = 0 ; m <= 14; m++)
 {
 if (m >= 10)
-{
 _putchar((m / 10) + 48);
 _putchar((m % 10) + 48);
-}
 }
 _putchar(10);
 }
