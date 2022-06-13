46ee63b54c844863fbb634e61104b59edfa5e8f7
Olalekan Okewale <olalekanokewale2020@gmail.com>
2022-06-13 22:44:38 +0000

    reverse printing of string

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ *
+ *
+ *
+ *
+ */
+void print_rev(char *s)
+{
+int l = 0;
+int m;
+while (*s != '\0')
+{
+l++;
+++s;
+}
+s--;
+for (m = l ; m > 0 ; m--)
+{
+_putchar(*s);
+s--;
+}
+_putchar(10);
+}
