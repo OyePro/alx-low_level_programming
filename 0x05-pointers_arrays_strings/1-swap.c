45053c7de84630040f15a0c49f171e3c74f1eff5
Olalekan Okewale <olalekanokewale2020@gmail.com>
2022-06-13 17:23:36 +0000

    swap them

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * swap_int - function to swap a and b
+ *@a: integer
+ *@b: second integer
+ *
+ * Return: void
+ */
+void swap_int(int *a, int *b)
+{
+*a = *b;
+*b = 'b';
+}
