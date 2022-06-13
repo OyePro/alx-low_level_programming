6205dbc2d9798d532e464ffa357935f2f221eaff
Olalekan Okewale <olalekanokewale2020@gmail.com>
2022-06-13 14:10:39 +0000

    reset to 98

---
+++ 0x05-pointers_arrays_strings/main.h
@@ -0,0 +1,21 @@
+#ifndef MAIN_H
+#define MAIN_H
+/**
+ * _putchar - writes the character c to the stdout
+ * @c: character to print
+ *
+ * Return: 1 (success)
+ * On error, return -1
+ */
+void reset_to_98(int *n);
+void swap_int(int *a, int *b);
+int _strlen(char *s);
+void _puts(char *str);
+void print_rev(char *s);
+void rev_string(char *s);
+void puts2(char *str);
+void puts_half(char *str);
+void print_array(int *a, int n);
+char *_strcpy(char *dest, char *src);
+int _atoi(char *s);
+#endif
