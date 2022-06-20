584f427acc8f402a5b319c1fdd4d81ae28bbf577
Olalekan Okewale <olalekanokewale2020@gmail.com>
2022-06-20 15:13:46 -0700

    memory area

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * _memcpy - function that copies memory area
+ * @dest: pointer to memory area
+ * @src: pointer to memory area
+ *
+ * Return: dest
+ */
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+unsigned int m;
+for (m = 0 ; m < n ; m++)
+{
+dest[m] = src[m];
+}
+return (dest);
+}
