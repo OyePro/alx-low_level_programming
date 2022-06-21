4cabff901e2001f09f6253309b1ea7e364964c95
Olalekan Okewale <olalekanokewale2020@gmail.com>
2022-06-20 18:27:51 -0700

    double pointer

---
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * set_string - function to set value of a pointer to another pointer
+ * @s - double pointer
+ * @to - character
+ *
+ * Return: void
+ */
+void set_string(char **s, char *to)
+{
+*s = to;
+}
