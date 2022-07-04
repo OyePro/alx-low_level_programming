cc04c15fed73c953e5ee523db90de312b578678a
Olalekan Okewale <olalekanokewale2020@gmail.com>
2022-07-04 22:19:10 +0000

    struct

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,16 @@
+#ifndef DOG_H
+#define DOG_H
+/**
+ * struct dog - structure dog contains different C type and their value
+ *
+ * @name: name of dog
+ * @owner: dog owner
+ * @age: age of dog
+ */
+struct dog
+{
+char *name;
+char *owner;
+float age;
+};
+#endif /* DOG_H */
