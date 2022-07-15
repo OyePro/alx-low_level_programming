063cd7dbb8d8654f54595fe0c7ad8bdfc311986f
Olalekan Okewale <olalekanokewale2020@gmail.com>
2022-07-15 10:22:57 +0000

    fixed

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -4,19 +4,27 @@
  * @n: numbers
  * @...: variable numbers
  *
- * Return: sum. else 0 if n == 0.
+ * Return: sum.
  */
 int sum_them_all(const unsigned int n, ...)
 {
 va_list ap;
-unsigned int sum = 0, m;
-/* int sum = 0;
-va_list ap; */
+unsigned int sum = 0, m = 0;
+/* int sum = 0; */
+/* va_list ap; */
+/* validate ap and initialize */
 if (n == 0)
+{
 return (0);
+}
 va_start(ap, n);
-for (m = 0; m < n ; m++)
+/* for (m = 0; m < n ; m++) */
+/* iterate through list, update sum, free list */
+while (m < n)
+{
 sum += va_arg(ap, int);
+m++;
+}
 va_end(ap);
 return (sum);
 }
